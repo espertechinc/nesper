483ab51335351d953d1e53e7405e0a0db875f034
Aaron Jackson <ajackson@revionics.com>
2016-10-02 23:13:09 -0500

    Updating version to 5.4.3

--- NEsper/NEsper/Properties/AssemblyInfo.cs
+++ NEsper/NEsper/Properties/AssemblyInfo.cs
@@ -40,7 +40,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("5.4.2")]
-[assembly: AssemblyFileVersion("5.4.2")]
+[assembly: AssemblyVersion("5.4.3")]
+[assembly: AssemblyFileVersion("5.4.3")]
 
 [assembly: InternalsVisibleTo("NEsper.Tests")]
